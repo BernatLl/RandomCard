143a7209b2c56bbe983ad739884ed1c231c0b34c
BernatLl <blloret@hotmail.com>
2022-01-20 17:42:30 +0000

    ww

--- src/app.js
+++ src/app.js
@@ -4,8 +4,34 @@ import "./main.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let pick;
+window.onload = () => {
+  document.querySelector(".spade").classList.add(GenRandomPicks());
+  document.querySelector(".cardNumber").innerHTML = GenRandomNumber();
+};
+let GenRandomNumber = () => {
+  let cardNumber = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let indexNumbers = Math.floor(Math.random() * cardNumber.length);
+  return cardNumber[indexNumbers];
+};
+let GenRandomPicks = () => {
+  let picks = ["diamond", "heart", "spade", "club"];
+
+  let indexPicks = Math.floor(Math.random() * picks.length);
+  return picks[indexPicks];
 };
